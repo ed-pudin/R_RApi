d524550b64742b97f1338eed0c6651fa2f28d6da
ed-pudin <ayedl851@gmail.com>
2023-07-07 18:42:15 -0600

    login

--- R&RApi/R&RApi/DataAccessLayer/Models/user.cs
+++ R&RApi/R&RApi/DataAccessLayer/Models/user.cs
@@ -3,10 +3,10 @@
     public class user
     {
         public string? id { get; set; }
-        public string name { get; set; }
-        public string lastname { get; set; }
+        public string? name { get; set; }
+        public string? lastname { get; set; }
         public string email { get; set; }
         public string password { get; set; }
-        public string rol { get; set; }
+        public string? rol { get; set; }
     }
 }
--- R&RApi/R&RApi/Program.cs
+++ R&RApi/R&RApi/Program.cs
@@ -1,8 +1,25 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+//Se añade el servicio de autentificacion
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).
+    AddJwtBearer(options =>
+    {
+        //Parametros de validacion 
+        options.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters
+        {
+            ValidateIssuerSigningKey = true,
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["JWT:Key"])),
+            ValidateIssuer = false, //No se valida este campo del token
+            ValidateAudience = false, //Ni este
+        };
+    });
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -18,6 +35,7 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+//Va aqui el middleware  
 app.UseAuthorization();
 
 app.MapControllers();
