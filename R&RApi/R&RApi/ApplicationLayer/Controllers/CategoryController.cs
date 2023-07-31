6ab82960cefe82d6774444c5204a7de5978d3a8b
ed-pudin <ayedl851@gmail.com>
2023-07-30 21:53:40 -0600

    Obtener categorias

---
+++ R&RApi/R&RApi/ApplicationLayer/Controllers/CategoryController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using R_RApi.ApplicationLayer.Servicios;
+using R_RApi.DataAccessLayer.Models;
+
+namespace R_RApi.ApplicationLayer.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CategoryController : ControllerBase
+    {
+        [HttpPost("AddCategory")]
+        public IActionResult addCategory(category data)
+        {
+            CategoryDAO catDAO = new CategoryDAO();
+            return Ok(catDAO.addCategory(data));
+        }
+        [HttpGet("GetCategories")]
+        public IActionResult getCategories()
+        {
+            CategoryDAO catDAO = new CategoryDAO();
+            return Ok(catDAO.getCategories());
+        }
+        [HttpPut("EditCategory/{id}")]
+        public IActionResult editCategory(string id, category data)
+        {
+            CategoryDAO catDAO = new CategoryDAO();
+            return Ok(catDAO.editCategory(id, data));
+        }
+    }
+}
---
+++ R&RApi/R&RApi/DataAccessLayer/Mapper/CategoryMapper.cs
@@ -0,0 +1,112 @@
+﻿using R_RApi.DataAccessLayer.Models;
+using R_RApi.DataAccessLayer.Queries;
+using R_RApi.DataAccessLayer.Response;
+using System.Data.SqlClient;
+
+namespace R_RApi.DataAccessLayer.Mapper
+{
+    public class CategoryMapper
+    {
+        SqlConnection _connection;
+        SqlCommand _command;
+        CategoryQuery _query;
+        SqlDataReader _reader;
+
+        public CategoryMapper()
+        {
+            Connection connection = new Connection();
+            _connection = connection.sqlCon();
+
+            _query = new CategoryQuery();
+        }
+        public ResponseApi addCategory(category c)
+        {
+            try
+            {
+                _connection.Open();
+                _command = new SqlCommand(_query.addCategory(), _connection);
+
+                _command.Parameters.AddWithValue("@name", c.name);
+
+                if (_command.ExecuteNonQuery() > 0)
+                {
+                    return new ResponseApi(1, 200, "OK", null);
+                }
+                else
+                {
+                    return new ResponseApi(0, 200, "Error al registrar", null);
+
+                }
+            }
+            catch (Exception ex)
+            {
+                return new ResponseApi(0, 400, ex.Message, null);
+            }
+
+        }
+        public ResponseApi editCategory(string id, category c)
+        {
+            try
+            {
+                _connection.Open();
+                _command = new SqlCommand(_query.editCategory(), _connection);
+
+                _command.Parameters.AddWithValue("@name", c.name);
+                _command.Parameters.AddWithValue("@id", id);
+
+                if (_command.ExecuteNonQuery() > 0)
+                {
+                    return new ResponseApi(1, 200, "OK", null);
+                }
+                else
+                {
+                    return new ResponseApi(0, 200, "Error al modificar", null);
+
+                }
+            }
+            catch (Exception ex)
+            {
+                return new ResponseApi(0, 400, ex.Message, null);
+            }
+
+        }
+        public ResponseApi getCategories()
+        {
+            try
+            {
+                List<category> categories = new List<category>();
+
+                _connection.Open();
+                _command = new SqlCommand(_query.getCategories(), _connection);
+
+                _reader = _command.ExecuteReader();
+
+                while (_reader.Read())
+                {
+                    category categoryM = new category
+                    {
+                        id = _reader["id"].ToString(),
+                        name = _reader["name"].ToString()
+                    };
+
+                    categories.Add(categoryM);
+                }
+                if (categories.Count > 0)
+                {
+                    return new ResponseApi(1, 200, "OK", categories.Cast<dynamic>().ToList());
+                }
+                else
+                {
+                    return new ResponseApi(0, 200, "No se encontraron categorias", null);
+
+                }
+
+            }
+            catch (Exception ex)
+            {
+                return new ResponseApi(0, 400, ex.Message, null);
+            }
+        }
+
+    }
+}
