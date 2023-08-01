6163df323550db8477113b4a3bf052b34b83cc17
Armando Carrizoza <32964497+LOrionJ10@users.noreply.github.com>
2023-07-31 17:32:17 -0700

    Registrar compra realizado correctamente

--- R&RApi/R&RApi/DataAccessLayer/Mapper/PurchaseMapper.cs
+++ R&RApi/R&RApi/DataAccessLayer/Mapper/PurchaseMapper.cs
@@ -1,4 +1,6 @@
-﻿using R_RApi.DataAccessLayer.Queries;
+﻿using R_RApi.DataAccessLayer.Models;
+using R_RApi.DataAccessLayer.Queries;
+using R_RApi.DataAccessLayer.Response;
 using System.Data.SqlClient;
 
 namespace R_RApi.DataAccessLayer.Mapper
@@ -18,23 +20,34 @@ namespace R_RApi.DataAccessLayer.Mapper
             _query = new PurchaseQuery();
         }
 
-        public string addPurchase(string service, float subTotalService, float subTotalProducts,
-                                    float payment, float total) 
+        public ResponseApi addPurchase(purchase pu) 
         {
-            _connection.Open();
-            _command = new SqlCommand(_query.addPurchase(), _connection);
+            try
+            {
+                _connection.Open();
+                _command = new SqlCommand(_query.addPurchase(), _connection);
 
-            _command.Parameters.AddWithValue("@service", service);
-            _command.Parameters.AddWithValue("@subtotalService", subTotalService);
-            _command.Parameters.AddWithValue("@subtotalProducts", subTotalProducts);
-            _command.Parameters.AddWithValue("@payment", payment);
-            _command.Parameters.AddWithValue("@total", total);
+                _command.Parameters.AddWithValue("@service", pu.service);
+                _command.Parameters.AddWithValue("@subtotalService", pu.subTotalService);
+                _command.Parameters.AddWithValue("@subtotalProducts", pu.subTotalProducts);
+                _command.Parameters.AddWithValue("@change", pu.change);
+                _command.Parameters.AddWithValue("@payment", pu.payment);
+                _command.Parameters.AddWithValue("@total", pu.total);
 
-            int rowsAffected = _command.ExecuteNonQuery();
+                if(_command.ExecuteNonQuery() > 0)
+                {
+                    return new ResponseApi(1, 200, "Ok", null);
+                }
+                else
+                {
+                    return new ResponseApi(0, 200, "Error al registrar", null);
+                }
 
-            return null;
-
-            //ESTO AUN NO ESTA LISTO
+            }
+            catch (Exception ex)
+            {
+                return new ResponseApi(0, 400, ex.Message, null);
+            }
         }
     }
 }
